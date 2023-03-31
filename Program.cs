99354a6c0d580973d3a8af3e8cf302ddb986ae85
MegaDen58 <extreme.den2004@gmail.com>
2023-03-31 11:13:49 +0300

    w

--- Program.cs
+++ Program.cs
@@ -1,58 +1,31 @@
 ﻿using System;
-
-class Class<T>
-{
-    public List<T> list = new List<T>();
-    public List<T> login = new List<T>();
-    public List<T> password = new List<T>();
-
-    public void addNumber(params T[] value)
-    {
-        foreach (T a in value)
-        {
-            list.Add(a);
-        }
-    }
-
-    public void deleteNumber(int number)
-    {
-        list.RemoveAt(number);
-    }
-
-    public T getNumber(int number)
-    {
-        return list[number];
-    }
-
-    public int getLenght()
-    {
-        return list.Count;
-    }
-}
-
-class User<T> : Class<T>
-{
-    public User(T login, T password) 
-    {
-        this.login.Add(login);
-        this.password.Add(password);
-    }
-}
-
+using System.Reflection;
 
 public class Program
 {
     public static void Main()
     {
-        Class<string> text = new Class<string>();
-        Class<int> number = new Class<int>();
-        Class<double> doubles = new Class<double>();
+        int x = 0;
+        string[] day = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятницу", "Суббота", "Воскресенье" };
 
-        text.addNumber("tom", "text", "college");
-        number.addNumber(5, 10, 20);
-        doubles.addNumber(1.35, 0.66, 122.1);
+        Message message = (string[] array, ref int index) =>
+        {
+            if(index == 6)
+            {
+                Console.WriteLine(array[index]);
+                index = 0;
+            }
+            else
+            {
+                Console.WriteLine(array[index]);
+                index += 1;
+            }
+        };
 
-        User<string> first = new User<string>("ggff", "password123");
-        User<string> second = new User<string>("login", "123");
+        for(int i = 0; i < 16; i++)
+        {
+            message(day, ref x);
+        }
     }
+    delegate void Message(string[] array, ref int index);
 }
